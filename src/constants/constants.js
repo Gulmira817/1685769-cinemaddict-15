1d3dcad8e361725777a079b4158c754e77b8cf20
Gulmira817 <38330620+Gulmira817@users.noreply.github.com>
2021-08-06 20:37:22 +0400

    remark module1-task3

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,15 +1,10 @@
 const BODY = document.querySelector('body');
 const HEADER = document.querySelector('header');
 const MAIN_ELEMENT = BODY.querySelector('.main');
-const FILM_LIST_ELEMENT = BODY.querySelector('.films-list');
-const FILMS_LIST_CONTAINER = BODY.querySelector('.films-list__container');
 const FOOTER_STATISTICS = BODY.querySelector('.footer__statistics');
 
 export {
-  MAIN_ELEMENT,
-  FILM_LIST_ELEMENT,
   HEADER,
-  FILMS_LIST_CONTAINER,
+  MAIN_ELEMENT,
   FOOTER_STATISTICS
-
 };
--- src/main.js
+++ src/main.js
@@ -7,34 +7,37 @@ import {
   filmCard,
   topRatingTemplate,
   topCommentsFilms,
-  stats,
   footerStatistics,
-  loading
+  films
 } from './index.js';
+
 import {
   HEADER,
   MAIN_ELEMENT,
-  FILM_LIST_ELEMENT,
-  FILMS_LIST_CONTAINER,
   FOOTER_STATISTICS
-} from './constants.js/constants.js';
+} from './constants/constants.js';
+
+const BODY = document.querySelector('body');
+
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 
 };
 
 render(HEADER, profileTemplate(), 'beforeend');
-render(MAIN_ELEMENT, filterTemplate(), 'afterbegin');
 render(MAIN_ELEMENT, menuTemplate(), 'afterbegin');
-render(MAIN_ELEMENT, filmDetailsTemplate(), 'beforeend');
-render(FILMS_LIST_CONTAINER, filmCard(), 'beforeend');
-render(FILMS_LIST_CONTAINER, filmCard(), 'beforeend');
-render(FILMS_LIST_CONTAINER, filmCard(), 'beforeend');
-render(FILMS_LIST_CONTAINER, filmCard(), 'beforeend');
-render(FILMS_LIST_CONTAINER, filmCard(), 'beforeend');
-render(FILMS_LIST_CONTAINER, showMoreButtonTemplate(), 'beforeend');
+render(MAIN_ELEMENT, filterTemplate(), 'beforeend');
+render(MAIN_ELEMENT, films(), 'beforeend');
+const FILMS_LIST_CONTAINER = BODY.querySelector('.films-list__container');
+for (let i = 0; i < 5; i++) {
+  render(FILMS_LIST_CONTAINER, filmCard(), 'beforeend');
+}
+const FILM_LIST_ELEMENT = BODY.querySelector('.films-list');
+render(FILM_LIST_ELEMENT, showMoreButtonTemplate(), 'beforeend');
+
 render(MAIN_ELEMENT, topRatingTemplate(), 'beforeend');
 render(MAIN_ELEMENT, topCommentsFilms(), 'beforeend');
-render(MAIN_ELEMENT, stats(), 'beforeend');
-render(FOOTER_STATISTICS, footerStatistics(), 'afterend');
-render(FILM_LIST_ELEMENT, loading(), 'afterbegin');
+render(FOOTER_STATISTICS, footerStatistics(), 'beforeend');
+render(BODY, filmDetailsTemplate(), 'beforeend');
+
+
